012590e5765c9e7ef70f6de67ab6174ef8a93cc0
Gregatron <thegregoryjohnson@gmail.com>
2020-01-27 19:37:34 -0500

    Adjusted service name for clarity.

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -1,12 +1,11 @@
-import { NotFoundError } from './../common/not-found-error';
-import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostService extends DataService {
+export class PortfolioService extends DataService {
 
   constructor(http: Http) {
     super('https://gitconnected.com/v1/portfolio/gregar12', http);
